0e04d2128c192379dd41c0af0b6758219802a909
FiveMonths2Full <FiveMonths2Full@outlook.com>
2025-06-30 18:09:54 -0400

    Refactor repeatString function to accept parameters and update tests to enable all cases

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let total = ""
+    if(num < 0) {return "ERROR"};
+    for (let i = 0; i < num; i++) {
+        total += str;
+    }
+    return total;
 };
 
 // Do not edit below this line
